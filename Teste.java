55aa8bce46956c4d4b14a442cf3cb9c96c0acef0
Guilherme Vicentini <guii.vicentini@hotmail.com>
2022-08-04 19:45:52 -0300

    Adicionando arquivo java para teste

---
+++ Teste.java
@@ -0,0 +1,12 @@
+public class Teste {
+    public static void main(String[] args) throws Exception {
+        char turma;
+        int valor;
+        System.out.println("Por favor, entre com a sua turma: ");
+        valor = System.in.read();
+        turma = (char)valor;
+        System.out.println("O valor lido foi: " + valor);
+        System.out.println("Esse valor representa o caractere: " + turma);
+            
+    }
+}
